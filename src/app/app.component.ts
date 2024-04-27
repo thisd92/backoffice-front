9b462eb2ad5fe0d3f51fd8b8b8ddcaef39ef99a3
thisd92 <80495413+thisd92@users.noreply.github.com>
2024-04-27 01:56:11 -0300

    update app component structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from './features/login/login.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @Component({
   selector: 'app-root',
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { UserRegisterComponent } from './pages/user-register/user-register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'user/register', component: UserRegisterComponent },
+];
