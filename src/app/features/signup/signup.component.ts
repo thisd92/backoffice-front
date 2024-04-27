7d65800d69ccaedf52aafa74a1e614163d16e0dd
thisd92 <80495413+thisd92@users.noreply.github.com>
2024-04-27 02:52:37 -0300

    fetch user register

--- src/app/features/signup/signup.component.ts
+++ src/app/features/signup/signup.component.ts
@@ -11,17 +11,27 @@ import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 export class SignupComponent {
   title = 'Sign Up';
   signupForm = this.formBuilder.group({
+    name: '',
+    email: '',
     username: '',
     password: '',
-    confirmPassword: '',
+    userRole: 'user',
   });
 
-  submitForm() {
-    console.log(this.signupForm.value);
+  async submitForm() {
+    await fetch('http://localhost:8090/user/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(this.signupForm.value),
+    });
     this.signupForm.patchValue({
+      name: '',
+      email: '',
       username: '',
       password: '',
-      confirmPassword: '',
+      userRole: 'user'
     });
   }
 
