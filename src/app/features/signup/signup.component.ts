7072fe1cbfa58b6f145ef19f30f0887758d679b1
thisd92 <80495413+thisd92@users.noreply.github.com>
2024-04-27 01:59:01 -0300

    update signup component

--- src/app/features/signup/signup.component.ts
+++ src/app/features/signup/signup.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.scss'
+  styleUrl: './signup.component.scss',
 })
 export class SignupComponent {
-  title = 'Sign Up'
+  title = 'Sign Up';
+  signupForm = this.formBuilder.group({
+    username: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  submitForm() {
+    console.log(this.signupForm.value);
+    this.signupForm.patchValue({
+      username: '',
+      password: '',
+      confirmPassword: '',
+    });
+  }
+
+  constructor(private formBuilder: FormBuilder) {}
 }
